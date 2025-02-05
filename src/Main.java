e3f585e49784a7bf830ac5f617e84e832a1416c9
B4YR1FF3R <siravit.47k@gmail.com>
2025-02-05 08:34:21 +0700

    Update

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        Character DOGE = new Character("DOGE");
+        DOGE.swingSword();
+        DOGE.swingSword();
+        DOGE.swingSword();
+        DOGE.swingSword();
+        DOGE.swingSword();
+        DOGE.takeDamage(30);
+        DOGE.takeDamage(20);
+        DOGE.takeDamage(50);
+        DOGE.rest();
+        DOGE.showDetails();
+    }
+}
